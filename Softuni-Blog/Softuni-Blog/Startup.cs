9aaab5dba53fb28bc119648c0637f49d3df7745b
grindypeach <vdimitrov13@yahoo.com>
2016-08-24 14:15:02 +0300

    Initial setup

---
+++ Softuni-Blog/Softuni-Blog/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(Softuni_Blog.Startup))]
+namespace Softuni_Blog
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
